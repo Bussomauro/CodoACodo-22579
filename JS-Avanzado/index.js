37c335fe0a18d22b28b979f400111b513092f2cc
Bussomauro <bussomauro@gmail.com>
2022-11-03 20:38:16 -0300

    ok

---
+++ JS-Avanzado/index.js
@@ -0,0 +1,7 @@
+console.log('hola mundo 1');
+
+setTimeout (() => {
+    console.log('hola mundo despues de 5 segundos');
+},5000); 
+
+console.log('hola mundo 2');
---
+++ nodeJs/index.js
@@ -0,0 +1,14 @@
+// const n1 =50;
+// const n2= 30;
+
+// console.log(n1+n2);
+
+// console.log('Hola Mundo');
+// console.log('Primer codigo NodeJs');
+
+// const calculadora = require('./calculadora.js');
+// console.log(calculadora.sumar(23,53));
+// console.log(calculadora.restar(59,25));
+
+const {default:swal}=require('sweetalert2');
+swal.fire('Hola mundo!!')
